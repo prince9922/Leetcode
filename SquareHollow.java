125bd9b5d3be5122f122f5a4b30deb490b7a5910
Prince Kumar <princesingh531@gmail.com>
2025-12-28 20:13:37 +0530

    Create SquareHollow.java

---
+++ SquareHollow.java
@@ -0,0 +1,30 @@
+package com.pattern;
+
+public class SquareHollow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=9;
+		
+		for (int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				
+				if (i==0 || i==n-1 || j==0 || j==n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
